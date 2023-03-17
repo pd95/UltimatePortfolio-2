7260fe80a1d0a35ec0a71c498f64b50c101c8ea2
Philipp <pd95@users.noreply.github.com>
2023-03-17 20:58:57 +0100

    Setup a CloudKit enabled CoreData model

--- UltimatePortfolio/UltimatePortfolioApp.swift
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 @main
 struct UltimatePortfolioApp: App {
+    @StateObject private var dataController = DataController()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+                .environmentObject(dataController)
         }
     }
 }
