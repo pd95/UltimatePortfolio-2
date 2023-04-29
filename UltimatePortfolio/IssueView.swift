66ce1453c10da9310353e6695c7fbc50ad8e326b
Philipp <pd95@users.noreply.github.com>
2023-04-29 11:51:31 +0200

    Enable copying issue title, marking as completed and reopen issue.

--- UltimatePortfolio/IssueView.swift
+++ UltimatePortfolio/IssueView.swift
@@ -75,6 +75,25 @@ struct IssueView: View {
         .onReceive(issue.objectWillChange) { _ in
             dataController.queueSave()
         }
+        .toolbar {
+            Menu {
+                Button {
+                    UIPasteboard.general.string = issue.title
+                } label: {
+                    Label("Copy Issue Title", systemImage: "doc.on.doc")
+                }
+
+                Button {
+                    issue.completed.toggle()
+                    dataController.save()
+                } label: {
+                    Label(issue.completed ? "Re-open Issue" : "Close Issue", systemImage: "bubble.left.and.exclamationmark.bubble.right")
+                }
+
+            } label: {
+                Label("Actions", systemImage: "ellipsis.circle")
+            }
+        }
     }
 }
 
