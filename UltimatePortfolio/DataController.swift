7c3b712eddd634eaccf20b71f88dc7775577fb16
Philipp <pd95@users.noreply.github.com>
2023-04-29 11:41:21 +0200

    Implement "Earning awards"

--- UltimatePortfolio/DataController.swift
+++ UltimatePortfolio/DataController.swift
@@ -217,4 +217,36 @@ class DataController: ObservableObject {
 
         selectedIssue = issue
     }
+
+    func count<T>(for fetchRequest: NSFetchRequest<T>) -> Int {
+        (try? container.viewContext.count(for: fetchRequest)) ?? 0
+    }
+
+    func hasEarned(award: Award) -> Bool {
+        switch award.criterion {
+        case "issues":
+            // return true if they added a certain number of issues
+            let fetchRequest = Issue.fetchRequest()
+            let awardCount = count(for: fetchRequest)
+            return awardCount >= award.value
+
+        case "closed":
+            // return true if they closed a certain number of issues
+            let fetchRequest = Issue.fetchRequest()
+            fetchRequest.predicate = NSPredicate(format: "completed = true")
+            let awardCount = count(for: fetchRequest)
+            return awardCount >= award.value
+
+        case "tags":
+            // return true if they created a certain number of tags
+            let fetchRequest = Tag.fetchRequest()
+            let awardCount = count(for: fetchRequest)
+            return awardCount >= award.value
+
+        default:
+            // an unknown award criterion; this should never be allowed
+            //fatalError("Unknown award criterion: \(award.criterion)")
+            return false
+        }
+    }
 }
