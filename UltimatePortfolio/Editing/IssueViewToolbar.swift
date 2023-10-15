d4a4b3fb9ecfeeee17dd920333adbbc5058197e2
Philipp <pd95@users.noreply.github.com>
2023-10-15 22:16:02 +0200

    Adding haptics when closing an issue. Three different versions: `.sensoryFeedback()` modifier for iOS 17, `UINotificationFeedbackGenerator` for standard haptics or `CoreHaptics` engine for fully custom haptics.

--- UltimatePortfolio/Editing/IssueViewToolbar.swift
+++ UltimatePortfolio/Editing/IssueViewToolbar.swift
@@ -5,11 +5,14 @@
 //  Created by Philipp on 28.05.23.
 //
 
+import CoreHaptics
 import SwiftUI
 
 struct IssueViewToolbar: View {
     @EnvironmentObject private var dataController: DataController
     @ObservedObject var issue: Issue
+
+    @State private var engine = try? CHHapticEngine()
 
     var openCloseButtonText: LocalizedStringKey {
         issue.completed ? "Re-open Issue" : "Close Issue"
@@ -23,12 +26,17 @@ struct IssueViewToolbar: View {
                 Label("Copy Issue Title", systemImage: "doc.on.doc")
             }
 
-            Button {
-                issue.completed.toggle()
-                dataController.save()
-            } label: {
+            Button(action: toggleCompleted) {
                 Label(openCloseButtonText, systemImage: "bubble.left.and.exclamationmark.bubble.right")
             }
+//            // iOS 17 way to trigger haptic feedback
+//            .sensoryFeedback(trigger: issue.completed) { _, newValue in
+//                if newValue {
+//                    .success
+//                } else {
+//                    nil
+//                }
+//            }
 
             Divider()
 
@@ -37,6 +45,52 @@ struct IssueViewToolbar: View {
             }
         } label: {
             Label("Actions", systemImage: "ellipsis.circle")
+        }
+    }
+
+    func toggleCompleted() {
+        issue.completed.toggle()
+        dataController.save()
+
+        // Before iOS 17, we use `UINotificationFeedbackGenerator` for haptic feedback or CoreHaptics
+        if issue.completed {
+            // UINotificationFeedbackGenerator().notificationOccurred(.success)
+
+            // Play a custom haptic using CoreHaptics engine
+            do {
+                try engine?.start()
+
+                let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 0)
+                let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+
+                let start = CHHapticParameterCurve.ControlPoint(relativeTime: 0, value: 1)
+                let end = CHHapticParameterCurve.ControlPoint(relativeTime: 1, value: 0)
+
+                let parameter = CHHapticParameterCurve(
+                    parameterID: .hapticIntensityControl,
+                    controlPoints: [start, end],
+                    relativeTime: 0
+                )
+
+                let event1 = CHHapticEvent(
+                    eventType: .hapticTransient,
+                    parameters: [intensity, sharpness],
+                    relativeTime: 0
+                )
+
+                let event2 = CHHapticEvent(
+                    eventType: .hapticContinuous,
+                    parameters: [sharpness, intensity],
+                    relativeTime: 0.125,
+                    duration: 1
+                )
+
+                let pattern = try CHHapticPattern(events: [event1, event2], parameterCurves: [parameter])
+                let player = try engine?.makePlayer(with: pattern)
+                try player?.start(atTime: 0)
+            } catch {
+                print("Playing haptics didn't work, but that's ok")
+            }
         }
     }
 }
